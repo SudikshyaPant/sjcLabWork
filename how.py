bd31eba150ad00bdaf18150e9630285e7303aae4
Abraham Chandafa <134372764+abrahamchandafa@users.noreply.github.com>
2023-05-30 16:29:51 +0800

    Update how.py

--- how.py
+++ how.py
@@ -2,6 +2,6 @@ print("Hello World!")
 print('Ta ta ta ta ta ta ta')
 print("WE NEED SOME ERRORS!")
 #Adding a greeting in swahili!
-print("We want problems")
+print("")
 print("I swear I'm up to no good")
 print("heoo")
