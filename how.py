900d806635b5c60e9970628dfa0ff42e88114747
SudikshyaPant <130307758+SudikshyaPant@users.noreply.github.com>
2023-05-30 16:12:24 +0800

    Update how.py

--- how.py
+++ how.py
@@ -2,7 +2,5 @@ print("Hello World!")
 print('vivre rire aimer')
 print("WE NEED SOME ERRORS!")
 #Adding a greeting in swahili!
-print("Habari!")
-print('Here comes chaos..!')
 print("We want problems")
 print("I swear I'm up to no good")
